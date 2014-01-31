7a94a2d16040c154baa4ed8988c83f4c623b0e69
Santi <uannight@gmail.com>
2014-01-31 11:31:37 +0100

    s

--- TaskDown/src/com/newteam/taskdown/LoginActivity.java
+++ TaskDown/src/com/newteam/taskdown/LoginActivity.java
@@ -3,6 +3,7 @@ package com.newteam.taskdown;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -36,6 +37,12 @@ public class LoginActivity extends Activity {
             errorDialog.show();	
         }
 	}
+	
+	public void onClickRegisterUser(View v) {
+		 Intent intent = new Intent(this, RegisterActivity.class);
+         startActivity(intent);
+	}
+	
 	private boolean validateUser(String username,String password)	{
 		if(username.equals("1") && password.equals("1"))	{
 			return true;
